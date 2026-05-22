e978be8274548415f3c61f76ec2ab624a25ae0d9
joyce162 <13638645701@163.com>
2026-05-22 14:59:57 +0800

    add auto code

--- dninter/src/main/java/com/dn/utils/Request.java
+++ dninter/src/main/java/com/dn/utils/Request.java
@@ -2,9 +2,7 @@ package com.dn.utils;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -46,6 +44,45 @@ public class Request {
         try {
             httpPost.setEntity(new StringEntity(body,"UTF-8"));
             CloseableHttpResponse execute = httpClient.execute(httpPost);
+            HttpEntity entity = execute.getEntity();
+            res = EntityUtils.toString(entity);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return res;
+    }
+
+    public static String httpPut(String url,String body){
+        HttpPut httpPut = new HttpPut(url);
+        String res = "";
+        try {
+            httpPut.setEntity(new StringEntity(body,"UTF-8"));
+            CloseableHttpResponse execute = httpClient.execute(httpPut);
+            HttpEntity entity = execute.getEntity();
+            res = EntityUtils.toString(entity);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return res;
+    }
+
+    public static String httpDelete(String url){
+        HttpDelete httpDelete = new HttpDelete(url);
+        String res = "";
+        try {
+            CloseableHttpResponse execute = httpClient.execute(httpDelete);
             HttpEntity entity = execute.getEntity();
             res = EntityUtils.toString(entity);
         }catch (Exception e){
